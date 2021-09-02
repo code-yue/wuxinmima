c3a672071dde6cad5df68c607b49ff61e89362d3
JACK.github.io <y2511308755.com@outlook.com>
2021-09-02 09:30:50 +0800

    数据库建立以及用户信息表建立

--- app/src/main/java/com/example/wuxinmima/Enroll.java
+++ app/src/main/java/com/example/wuxinmima/Enroll.java
@@ -2,15 +2,48 @@ package com.example.wuxinmima;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class Enroll extends AppCompatActivity {
+public class Enroll extends AppCompatActivity implements View.OnClickListener{
+    private EditText 注册用户名;
+    private EditText 注册邮箱 ;
+    private EditText 注册密码;
+    private EditText 注册手机号 ;
+    private Button 注册登录 ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
+         注册用户名 = (EditText)findViewById(R.id.用户名);
+         注册邮箱 = (EditText)findViewById(R.id.邮箱);
+         注册密码 = (EditText)findViewById(R.id.注册密码);
+         注册手机号 = (EditText)findViewById(R.id.注册手机号);
+         注册登录 = (Button)findViewById(R.id.注册登录);
+        注册登录.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.注册登录:
+                String 用户名 = 注册用户名.getText().toString();
+                String 邮箱 = 注册邮箱.getText().toString();
+                String 密码 = 注册密码.getText().toString();
+                String 手机号 = 注册手机号.getText().toString();
+                Users users = new Users();
+                users.set用户名(用户名);
+                users.set邮箱(邮箱);
+                users.set密码(密码);
+                users.set手机号(手机号);
+                users.save();
+                Intent intent = new Intent(Enroll.this,MainActivity.class);
+                startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/wuxinmima/Users.java
@@ -0,0 +1,52 @@
+package com.example.wuxinmima;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Users extends LitePalSupport {
+    private int id;
+    private String 邮箱;
+    private String 用户名;
+    private String 密码;
+    private String 手机号;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void set用户名(String 用户名) {
+        this.用户名 = 用户名;
+    }
+
+    public String get用户名() {
+        return 用户名;
+    }
+
+    public void set邮箱(String 邮箱) {
+        this.邮箱 = 邮箱;
+    }
+
+    public String get邮箱() {
+        return 邮箱;
+    }
+
+    public void set密码(String 密码) {
+        this.密码 = 密码;
+    }
+
+    public String get密码() {
+        return 密码;
+    }
+
+    public void set手机号(String 手机号) {
+        this.手机号 = 手机号;
+    }
+
+    public String get手机号() {
+        return 手机号;
+    }
+    
+}
